5fdd813313fd4a4c46b0039c41544932af7311b1
zsmith27 <zsmith2727@gmail.com>
2021-07-15 09:42:21 -0400

    ITS requested input and output data.

---
+++ inst/requests/2021-07-14_examples-for-its.R
@@ -0,0 +1,19 @@
+r2004299 <- read_als(zip_path = here::here("inst",
+                                           "example_zips",
+                                           "R2004299.zip"))
+
+export_json(
+  x = r2004299 ,
+  path = "C:/Users/zmsmith.000/OneDrive - New York State Office of Information Technology Services/Desktop/examples",
+  filename = "r2004299"
+)
+
+r2005223 <- read_als(zip_path = here::here("inst",
+                                           "example_zips",
+                                           "R2005223.zip"))
+
+export_json(
+  x = r2005223 ,
+  path = "C:/Users/zmsmith.000/OneDrive - New York State Office of Information Technology Services/Desktop/examples",
+  filename = "r2005223"
+)
