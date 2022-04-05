a80f5edbbdda39281193f04ae34a94fdf0e7e46a
zsmith27 <zsmith2727@gmail.com>
2022-04-05 11:14:03 -0400

    example of a flattened als DF.

---
+++ data-raw/flattened_als_df.R
@@ -0,0 +1,7 @@
+## code to prepare `DATASET` dataset goes here
+extracted_list <- Sentry::read_als(zip_path = here::here("inst",
+                                                         "example_zips",
+                                                         "R2004299.zip"))
+
+flattened_als_df <- Sentry::flatten_als(x = extracted_list)
+usethis::use_data(flattened_als_df , overwrite = TRUE)
