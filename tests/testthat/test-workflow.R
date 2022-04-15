bf6207514fa4f3a853cdc09533e7aa92154ebcc7
zsmith27 <zsmith2727@gmail.com>
2022-04-15 16:33:18 -0400

    Minor updates to names.

--- tests/testthat/test-workflow.R
+++ tests/testthat/test-workflow.R
@@ -1,11 +1,11 @@
-test_that("workflow", {
+test_that("ALS workflow", {
   r2004299 <- read_als(zip_path = here::here("inst",
                                              "example_zips",
                                              "R2004299.zip"))
   temp_path <- tempdir()
   on.exit(unlink(temp_path))
 
-  workflow(
+  als_wf <- workflow(
     x = r2004299,
     filepath = temp_path,
     filename = "R2004299"
