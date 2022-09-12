b7addb2f662d72fada7367c5f370a268514e57fd
zsmith27 <zsmith2727@gmail.com>
2022-09-12 16:48:41 -0400

    Added examples to test for missing data handling.

--- tests/testthat/test-workflow.R
+++ tests/testthat/test-workflow.R
@@ -18,3 +18,77 @@ test_that("ALS workflow", {
 
 })
 
+
+
+test_that("ALS workflow: Missing Result Table", {
+
+ no_result <- read_als(zip_path = here::here("inst",
+                                             "example_zips",
+                                             "R2004299",
+                                             "no_result.zip"))
+  temp_path <- tempdir()
+  on.exit(unlink(temp_path))
+
+  wf <- workflow(
+    x = no_result,
+    filename = "R2004299",
+    gen_report = FALSE
+  )
+
+  testthat::expect_equal(
+    wf$report$message,
+    "The ALS SDG, R2004299, did not pass the automated validation for the following reason(s): * 66 name(s) missing from Result Table."
+  )
+
+  testthat::expect_null(wf$data)
+
+})
+
+
+test_that("ALS workflow: Missing Batch Table", {
+
+  no_batch <- read_als(zip_path = here::here("inst",
+                                              "example_zips",
+                                              "R2004299",
+                                              "no_batch.zip"))
+  temp_path <- tempdir()
+  on.exit(unlink(temp_path))
+
+  wf <- workflow(
+    x = no_batch,
+    filename = "R2004299",
+    gen_report = FALSE
+  )
+
+  testthat::expect_equal(
+    wf$report$message,
+    "The ALS SDG, R2004299, did not pass the automated validation for the following reason(s): * 8 name(s) missing from Batch Table."
+  )
+
+  testthat::expect_null(wf$data)
+
+})
+
+test_that("ALS workflow: Missing Sample Table", {
+
+  no_sample <- read_als(zip_path = here::here("inst",
+                                             "example_zips",
+                                             "R2004299",
+                                             "no_sample.zip"))
+  temp_path <- tempdir()
+  on.exit(unlink(temp_path))
+
+  wf <- workflow(
+    x = no_sample,
+    filename = "R2004299",
+    gen_report = FALSE
+  )
+
+  testthat::expect_equal(
+    wf$report$message,
+    "The ALS SDG, R2004299, did not pass the automated validation for the following reason(s): * 29 name(s) missing from Sample Table."
+  )
+
+  testthat::expect_null(wf$data)
+
+})
